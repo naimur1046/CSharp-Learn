5649257c24ea316655153c7da1f2fb25eb9b9f6b
naimur rahman rumel <naimurrahman1046@gmail.com>
2024-10-06 14:53:57 +0600

    Add project files.

---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,25 @@
+﻿var stars = new GithubService().getStars("throwx");
+
+class GithubService
+{
+    private IEGithubClient _githubClient;
+    public int getStars(String repoName )
+    {
+
+        return _githubClient.GetRepo(repoName).Stars;
+    }
+}
+
+public interface IEGithubClient
+{
+     (String repoName,int Stars) GetRepo(String repoName);
+}
+
+
+class GithubClient:IEGithubClient
+{
+    public (String repoName, int Stars) GetRepo(String repoName)
+    {
+        return ( repoName, repoName.Length);
+    }
+}
